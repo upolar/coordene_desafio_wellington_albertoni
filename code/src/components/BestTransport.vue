c1a28efe071288cfd7a0f2bacefa32a98d1805df
Wellington Campos <wellington.albertoni@gmail.com>
2022-07-14 09:17:00 -0400

    NOVO: Adicionado formulario com menu de cidades e campo de peso
    	- Obtem da chamada de API os valores únicos de cidade
    	- Adicionado menu dropdown gerado com o nome das cidades
    	- Adicionado campo de inserção para o peso
    	- Adicionado botão 'analisar'.

--- code/src/components/BestTransport.vue
+++ code/src/components/BestTransport.vue
@@ -1,11 +1,38 @@
 <template>
-  <div class="title">
-    <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand class="ml-2">
-        <b>{{ appName }}</b>
-      </b-navbar-brand>
-    </b-navbar>
+  <div class="bestTransport">
+    <div class="title">
+        <b-navbar toggleable="lg" type="dark" variant="info">
+          <b-navbar-brand class="ml-2">
+            <b>{{ appName }}</b>
+          </b-navbar-brand>
+        </b-navbar>
+      </div>
+
+    <div id="frete" class="frete">
+      <div class="forms">
+
+        <div class="destino">
+          <h4>Destino</h4>
+          <select v-model="selected">
+            <option disabled value="" selected>Escolha uma cidade</option>
+            <option v-for="city in cities" :key="city" required> {{city}}</option>
+          </select> 
+        </div> 
+
+        <div class="peso">
+          <h4>Peso</h4>
+          <input v-model.number="peso"/>
+        </div>
+
+        <button v-on:click="getAlternatives">Analisar</button>
+
+        <div id="result"></div>
+      </div>
+      
+    </div>
+
   </div>
+  
 
 </template>
 
@@ -21,18 +48,21 @@ export default {
     BNavbarBrand,
   },
   data() {
-    const appName = ''
+      const appName = ''
 
     return {
       appName,
+      selected: null,
+      transport: [],
+      peso: 0,
       cities: []
     }
   },
   created() {
     // Implemente aqui o GET dos dados da API REST
     // para que isso ocorra na inicialização da pagina
-    this.appName = 'Melhor Frete'
-    let target = 'http://localhost:3000/transport'
+    this.appName = 'Melhor Frete';
+    let target = 'http://localhost:3000/transport';
 
     const options = {
       method: 'GET',
@@ -40,16 +70,37 @@ export default {
         Accept: "application/json",
         "Content-Type": "application/json; charset=utf-8",
       }
-    }
+    };
     
-    this.cities = fetch(target, options)
-      .then((response) => response.json())
+    fetch(target, options)
+      .then((response) =>
+          response.json())
+      .then((data) => {
+          this.transport = data
+          this.loadCitiesList()
+        });
   },
   methods: {
     // Implemente aqui os metodos utilizados na pagina
-    methodFoo() {
-      console.log(this.appName)
-    },
+    async loadCitiesList() {
+      /* Carrega as cidades (sem repetir elementos) em um objeto Set e
+       armazena em this.cities */
+      let cities = new Set()
+
+      try {
+        let data = await this.transport;
+        data.forEach(function(valor) {
+            cities.add(valor['city'])
+          }
+        )
+      } catch (err){
+        console.error(err)
+      }
+
+      this.cities = cities
+   },
+   getAlternatives() {
+   }
   },
 }
 </script>
